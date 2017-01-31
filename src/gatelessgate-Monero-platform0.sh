81b8585e87a4c636c780b899e86fe21f0f9ea8bf
zawawawa <zawawawawawawawawa@gmail.com>
2017-01-31 00:41:23 -0800

    Fix build issues on Linux.

---
+++ src/gatelessgate-Monero-platform0.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+GPU_FORCE_64BIT_PTR=1 GPU_MAX_HEAP_SIZE=100 GPU_USE_SYNC_OBJECTS=1 GPU_MAX_ALLOC_PERCENT=100 GPU_SINGLE_ALLOC_PERCENT=100 ./gatelessgate --gpu-platform 0 -k cryptonight -o stratum+tcp://pool.minexmr.com:4444 -u 4BFrrjpBt9vF35MhqQW8KL6pTEu6W1WosXDJqgFDzA7PT7McN9uUkYmNnemDvd6wWBgrYdUP9tb2pLYsZ8Qxx7HqHc7FGAP -p x --rawintensity 720 --worksize 4 --gpu-threads 1 --no-extranonce
---
+++ src/gatelessgate-Zcash-platform1.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+GPU_FORCE_64BIT_PTR=1 GPU_MAX_HEAP_SIZE=100 GPU_USE_SYNC_OBJECTS=1 GPU_MAX_ALLOC_PERCENT=100 GPU_SINGLE_ALLOC_PERCENT=100 ./gatelessgate -k equihash --gpu-threads 2 --gpu-platform 1 -o stratum+tcp://us1-zcash.flypool.org:3333 -u t1NwUDeSKu4BxkD58mtEYKDjzw5toiLfmCu -p z
+
