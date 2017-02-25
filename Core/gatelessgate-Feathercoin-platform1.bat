c8717a768a59e9afdbf5178d5322140ae9cb1673
zawawawa <zawawawawawawawawa@gmail.com>
2017-02-25 15:10:07 -0800

    Optimize NeoScrypt kernel.

--- Core/gatelessgate-Feathercoin-platform1.bat
+++ Core/gatelessgate-Feathercoin-platform1.bat
@@ -4,5 +4,5 @@
 @set GPU_USE_SYNC_OBJECTS 1
 @set GPU_MAX_ALLOC_PERCENT 100
 @set GPU_SINGLE_ALLOC_PERCENT 100
-gatelessgate.exe -k neoscrypt --gpu-platform 1 -o s1.theblocksfactory.com:3333 -u zawawa.gatelessgate_128 -p x -I 16 -w 256 -g 1
+gatelessgate.exe -k neoscrypt --gpu-platform 1 -o s1.theblocksfactory.com:3333 -u zawawa.gatelessgate_128 -p x -I 13 -w 256 -g 1
 pause
--- Core/gatelessgate-Feathercoin-platform1.sh
+++ Core/gatelessgate-Feathercoin-platform1.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-GPU_FORCE_64BIT_PTR=1 GPU_MAX_HEAP_SIZE=100 GPU_USE_SYNC_OBJECTS=1 GPU_MAX_ALLOC_PERCENT=100 GPU_SINGLE_ALLOC_PERCENT=100 ./gatelessgate -k neoscrypt --gpu-platform 1 -o s1.theblocksfactory.com:3333 -u zawawa.gatelessgate_128 -p x -I 16 -w 256 -g 1
+GPU_FORCE_64BIT_PTR=1 GPU_MAX_HEAP_SIZE=100 GPU_USE_SYNC_OBJECTS=1 GPU_MAX_ALLOC_PERCENT=100 GPU_SINGLE_ALLOC_PERCENT=100 ./gatelessgate -k neoscrypt --gpu-platform 1 -o s1.theblocksfactory.com:3333 -u zawawa.gatelessgate_128 -p x -I 13 -w 256 -g 1
 
