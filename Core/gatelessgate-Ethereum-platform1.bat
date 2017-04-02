52d3f41aae554e8cd48bdfa15136dbc5f90430bf
zawawawa <zawawawawawawawawa@gmail.com>
2017-04-01 23:10:36 -0700

    Change default settings for Ethereum.

--- Core/gatelessgate-Ethereum-platform1.bat
+++ Core/gatelessgate-Ethereum-platform1.bat
@@ -4,5 +4,5 @@
 @set GPU_USE_SYNC_OBJECTS 1
 @set GPU_MAX_ALLOC_PERCENT 100
 @set GPU_SINGLE_ALLOC_PERCENT 100
-gatelessgate.exe --gpu-platform 1 -k ethash-new -o stratum+tcp://eu1.ethermine.org:4444 -u 0x91fa32e00b0f365d629fb625182a83fed61f0642.gatelessgate -p x --xintensity 4620 --worksize 192 --gpu-threads 2 --no-extranonce
+gatelessgate.exe --gpu-platform 1 -k ethash-new -o stratum+tcp://eu1.ethermine.org:4444 -u 0x91fa32e00b0f365d629fb625182a83fed61f0642.gatelessgate -p x --xintensity 4608 --worksize 192 --gpu-threads 2 --no-extranonce
 pause
